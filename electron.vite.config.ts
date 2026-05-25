ff04fb4ac5b99daec9d3ea0156cc550545eb1449
wangyan <1392253739@qq.com>
2026-05-25 14:44:45 +0800

    chore: add TypeScript, electron-vite, and Vitest configuration

---
+++ electron.vite.config.ts
@@ -0,0 +1,38 @@
+import { resolve } from 'path'
+import { defineConfig } from 'electron-vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  main: {
+    build: {
+      outDir: 'out/main',
+      rollupOptions: {
+        input: {
+          index: resolve(__dirname, 'src/main/index.ts')
+        }
+      }
+    }
+  },
+  preload: {
+    build: {
+      outDir: 'out/preload',
+      rollupOptions: {
+        input: {
+          index: resolve(__dirname, 'src/preload/index.ts')
+        }
+      }
+    }
+  },
+  renderer: {
+    root: resolve(__dirname, 'src/renderer'),
+    build: {
+      outDir: resolve(__dirname, 'out/renderer'),
+      rollupOptions: {
+        input: {
+          index: resolve(__dirname, 'src/renderer/index.html')
+        }
+      }
+    },
+    plugins: [react()]
+  }
+})
